75bfbbe7900d69361516ab0bcd8da0a91981b042
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-04-20 15:27:24 -0300

    Oscar_Dev pruebaflow2/index.js Añadi cosas Oscar

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const  sum = (a,b) => a+b
 console.log(sum(2,4))
 
+const oscarfunc = () =>{
+    console.log("Weee")
+}
 const log = ()=>{
     console.log("esto es un sdfsd")
 }
-
-//Aca oscar cambio algo//
