d32439f5a7382e1a25a0b97c8625e5693bd21869
ppalmav <ppalmav@users.noreply.github.com>
2023-04-20 14:19:38 -0400

    Pablo_Dev: agrego modulo functions, package.json, multiplicar y mostrar restar

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import  {multiplicar} from './functions.js'
+
 const sum = (a, b) => a + b
 console.log(sum(2, 4))
 
@@ -9,3 +11,6 @@ const restar = (a, b) => {
     return a - b * 2
 
 }
+console.log(restar(5,5))
+
+console.log(multiplicar(5,5))
