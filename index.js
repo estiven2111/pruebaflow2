7866b4d90d4459b6d410a7f56c53ddbcbc2928b3
Alan Winkler <alanquimeywinkler@gmail.com>
2023-04-20 16:23:04 -0300

    Alan generó un cambio

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ console.log(sum(2,4))
 const log = ()=>{
     console.log("esto es un sdfsd")
 }
+const log1 = ()=>{
+    console.log("esto es un sdfsd")
+}
+// Acá hice un cambio
+const variable = 1;
